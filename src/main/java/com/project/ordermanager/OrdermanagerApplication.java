e9bb3bdd48194dfd928bc7b1d17b2d241d53c8fa
 slevenet <Seeattackwin32>
2018-11-14 04:54:06 +0300

    Initial commit

---
+++ src/main/java/com/project/ordermanager/OrdermanagerApplication.java
@@ -0,0 +1,12 @@
+package com.project.ordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdermanagerApplication.class, args);
+    }
+}
